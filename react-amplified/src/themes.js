debd3a0069c5edf719c475c4dd8231e47332dad5
unknown <kati_packer@hotmail.com>
2022-07-08 16:15:32 +0100

    completed it!

---
+++ react-amplified/src/themes.js
@@ -0,0 +1,11 @@
+ const styles = {
+      container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20, backgroundColor:'lavender' },
+      todo: {  marginBottom: 15, },
+      input: { border: 'none', backgroundColor: '#FFDFD3', marginBottom: 10, padding: 0, fontSize: 14 },
+      todoName: { fontSize: 14, fontWeight: 'bold' },
+      todoDescription: { marginBottom: 0, marginTop: -10},
+      button: { backgroundColor: '#E0BBE4', color: 'white', outline: 'none', fontSize: 18, 
+      padding: '12px 0px' }
+    
+    }
+    export default styles
